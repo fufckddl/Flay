5d96f0cf9112d6510f190664b2193f8475a95690
Lee ChangRyoel <dlckdfuf141@naver.com>
2024-11-13 17:53:56 +0900

    add: background filter

---
+++ loadBackground.py
@@ -0,0 +1,4 @@
+import cv2;
+
+# 가상 배경 이미지 불러오기 및 크기 조정
+beach_image = cv2.imread("./images/beach.jpg")
--- loadPredictor.py
+++ loadPredictor.py
@@ -1,5 +1,10 @@
 import dlib
+import mediapipe as mp
 
 # 얼굴 탐지기와 랜드마크 예측기 초기화
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+# MediaPipe Selfie Segmentation 초기화
+mp_selfie_segmentation = mp.solutions.selfie_segmentation
+segmentation = mp_selfie_segmentation.SelfieSegmentation(model_selection=1)
